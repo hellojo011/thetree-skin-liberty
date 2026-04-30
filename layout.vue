79cc73870a38e247cb7733db96bf063ffd883350
hellojo011 <hellojo011@hotmail.co.kr>
2026-05-01 01:02:11 +0900

    라이선스 링크 추가

--- layout.vue
+++ layout.vue
@@ -160,7 +160,7 @@
                     <ul class="footer-places" @click="onDynamicContentClick($event)" v-html="$store.state.config['skin.liberty.footer_html'] || $store.state.config['wiki.footer_text']" />
                     <ul class="footer-icons">
                         <li class="footer-poweredbyico">
-                            <a href="//github.com/wjdgustn/thetree-skin-liberty" target="_blank">Liberty</a> | <a href="//github.com/wjdgustn/thetree" target="_blank">the tree</a>
+                            <a href="//github.com/hellojo011/thetree-skin-liberty" target="_blank">Liberty</a> | <a href="//github.com/wjdgustn/thetree" target="_blank">the tree</a>
                         </li>
                     </ul>
                 </div>
