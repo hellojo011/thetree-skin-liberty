73a550cf1a841c9bbdab8f83a86d83acc8e17448
hellojo011 <hellojo011@hotmail.co.kr>
2026-08-08 23:31:42 +0900

    not found 이미지 미출력 버그 수정

--- layout.vue
+++ layout.vue
@@ -160,7 +160,7 @@
                     <ul class="footer-places" @click="onDynamicContentClick($event)" v-html="$store.state.config['skin.liberty.footer_html'] || $store.state.config['wiki.footer_text']" />
                     <ul class="footer-icons">
                         <li class="footer-poweredbyico">
-                            <a href="//github.com/hellojo011/thetree-skin-liberty" target="_blank">Liberty</a> | <a href="//github.com/wjdgustn/thetree" target="_blank">the tree</a>
+                            <a href="//github.com/hellojo011/thetree-skin-liberty" target="_blank">Liberty Edited by DIGIWB</a> | <a href="//github.com/wjdgustn/thetree" target="_blank">the tree</a>
                         </li>
                     </ul>
                 </div>
@@ -225,8 +225,16 @@ export default {
 	    this.showProfileImage();
 		initEasterEgg();
         initKonamiEasterEgg();
+		this.$nextTick(() => {
+            // 초기 페이지 로드 시에도 실행되도록
+            this.updateNotFoundImage(this.$store.state.page.viewName)
+        })
+
 	},
     watch: {
+		'$store.state.page.viewName'(newVal) {
+            this.$nextTick(() => this.updateNotFoundImage(newVal))
+        },
         $route() {
             this.isShowACLMessage = false;
         },
@@ -289,7 +297,7 @@ export default {
                 const alertBox = article.querySelector('.thetree-alert')
                 let imgElement = `<img id="notfound-watermark"
                        src="${randomSrc}"
-                       style="width: 300px; opacity: 0.4; right: 0px; padding-right: 10px; margin-right: 20px; z-index: 0; position: absolute; pointer-events:none;">`
+                       style="width: 250px; opacity: 0.4; right: 0px; padding-right: 10px; margin-right: 20px; z-index: 0; position: absolute; pointer-events:none;">`
                 if (alertBox) {
                     alertBox.insertAdjacentHTML(
 					  'afterend',
